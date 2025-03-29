ddc3acfcf105418fef38b70f61ab5bf43af095bd
abraham-heuy <113937812+abraham-heuy@users.noreply.github.com>
2025-03-29 18:31:15 +0300

    changes...

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  onlogin() {  
+
+    // Replace 'your_app_id' with your actual app ID  
+    const appId = '70645';  
+    const redirectUrl = `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}`;  
+
+    // Redirect to the login page  
+    window.location.href = redirectUrl;  
+}  
+onregister(){
+  const redirectUrl = `https://hub.deriv.com/tradershub/signup`; 
+  window.location.href = redirectUrl; 
+
+}
 
 }
